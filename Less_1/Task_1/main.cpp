8d89eee99fd1fb2f7c8781fb05b73f800a201ca2
“Student” <Student>
2024-12-22 18:16:14 +0300

    Add Less_1, Task_1 and Task_2

---
+++ Less_1/Task_1/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <thread>
+
+int maxClients = 5;
+int clientCounter = 0;
+
+void clientThread() {
+    while (clientCounter < maxClients) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        clientCounter++;
+        std::cout << "Клиент встал в очередь, текущее число клиентов: " << clientCounter << std::endl;
+    }
+}
+
+void operatorThread() {
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        if (clientCounter > 0) {
+            clientCounter--;
+            std::cout << "Клиент обслужен, текущее число клиентов: " << clientCounter << std::endl;
+        } else {
+            std::cout << "Оператор закончил работу, текущее число клиентов: " << clientCounter << std::endl;
+            break;
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+    std::thread client(clientThread);
+    std::thread operatorT(operatorThread);
+
+    client.join();
+    operatorT.join();
+
+    return 0;
+}
