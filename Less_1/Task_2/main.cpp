16d67cea42a5513f484feea31cb38a71ca6909a0
“Student” <Student>
2025-01-07 16:51:28 +0300

    Change main.cpp

--- Less_1/Task_2/main.cpp
+++ Less_1/Task_2/main.cpp
@@ -2,14 +2,20 @@
 #include <thread>
 #include <vector>
 #include <iomanip>
+#include <chrono>
+#include <mutex>
 
-static void sumVectors(double sumOfVectors, const std::vector<int> newVector1, const std::vector<int> newVector2) {
-    for (int i = 0; i < newVector1.size(); ++i) {
-        sumOfVectors += newVector1.at(i) + newVector2.at(i);
+void sumVectors(double &sumOfVectors, const std::vector<int> &newVector1, const std::vector<int> &newVector2,
+                std::mutex &mtx) {
+    double localSum = 0;
+    for (size_t i = 0; i < newVector1.size(); ++i) {
+        localSum += newVector1.at(i) + newVector2.at(i);
     }
+    std::lock_guard<std::mutex> lock(mtx);
+    sumOfVectors += localSum;
 }
 
-int main(int argc, char **argv) {
+int main() {
     std::cout << "Количество аппаратных ядер: " << std::thread::hardware_concurrency() << "\n\n";
     std::cout << std::setw(12) << "\t\t   1000" << "\t\t    10000" << "\t       100000" << "\t     1000000" << "\n";
 
@@ -19,40 +25,29 @@ int main(int argc, char **argv) {
     std::vector<int> sizeOfVector{1000, 10000, 100000, 1000000};
 
     for (auto &countThread : numOfThreads) {
-        if (countThread == 1) {
-            std::cout << std::endl << countThread << " поток";
-        } else if (countThread == 2 || countThread == 4) {
-            std::cout << std::endl << countThread << " потока";
-        } else {
-            std::cout << std::endl << countThread << " потоков";
-        }
+        std::cout << std::endl << countThread
+                  << (countThread == 1 ? " поток" : (countThread < 5 ? " потока" : " потоков"));
 
         for (auto &countVector : sizeOfVector) {
             vector1.resize(countVector, 1);
             vector2.resize(countVector, 2);
             std::vector<std::thread> threads;
             double sumOfVectors = 0;
+            std::mutex mtx;
 
             int partOfTheSize = static_cast<int>(countVector / countThread);
-
             auto start = std::chrono::high_resolution_clock::now();
+
             for (int i = 0; i < countThread; i++) {
-                std::vector<int> newVector1;
-                std::vector<int> newVector2;
-                int rightBorder = 0;
+                std::vector<int> newVector1(vector1.begin() + i * partOfTheSize, vector1.begin() +
+                                                                                 (i == countThread - 1 ? countVector : (i + 1) * partOfTheSize));
+                std::vector<int> newVector2(vector2.begin() + i * partOfTheSize, vector2.begin() +
+                                                                                 (i == countThread - 1 ? countVector : (i + 1) * partOfTheSize));
 
-                if (i != countThread - 1) {
-                    rightBorder = partOfTheSize * (i + 1);
-                } else {
-                    rightBorder = countVector;
-                }
-
-                for (int j = partOfTheSize * i; j < rightBorder; ++j) {
-                    newVector1.push_back(vector1.at(j));
-                    newVector2.push_back(vector2.at(j));
-                }
-                threads.push_back(std::thread(sumVectors, std::ref(sumOfVectors), newVector1, newVector2));
+                threads.push_back(
+                        std::thread(sumVectors, std::ref(sumOfVectors), newVector1, newVector2, std::ref(mtx)));
             }
+
             for (auto &it : threads) {
                 it.join();
             }
