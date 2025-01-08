539ba9bffb062a98ac3ba7b8c0acc8d2692c5244
“Student” <Student>
2025-01-09 01:44:33 +0300

    Upd_main.cpp

--- Less_1/Task_2/main.cpp
+++ Less_1/Task_2/main.cpp
@@ -3,24 +3,18 @@
 #include <vector>
 #include <iomanip>
 #include <chrono>
-#include <mutex>
 
-void sumVectors(double &sumOfVectors, const std::vector<int> &newVector1, const std::vector<int> &newVector2,
-                std::mutex &mtx) {
-    double localSum = 0;
-    for (size_t i = 0; i < newVector1.size(); ++i) {
-        localSum += newVector1.at(i) + newVector2.at(i);
+void sumVectors(const std::vector<int> &vector1, const std::vector<int> &vector2, std::vector<int> &resultVector,
+                size_t start, size_t end) {
+    for (size_t i = start; i < end; ++i) {
+        resultVector[i] = vector1[i] + vector2[i];
     }
-    std::lock_guard<std::mutex> lock(mtx);
-    sumOfVectors += localSum;
 }
 
 int main() {
     std::cout << "Количество аппаратных ядер: " << std::thread::hardware_concurrency() << "\n\n";
     std::cout << std::setw(12) << "\t\t   1000" << "\t\t    10000" << "\t       100000" << "\t     1000000" << "\n";
 
-    std::vector<int> vector1;
-    std::vector<int> vector2;
     std::vector<int> numOfThreads{1, 2, 4, 8, 16};
     std::vector<int> sizeOfVector{1000, 10000, 100000, 1000000};
 
@@ -29,23 +23,20 @@ int main() {
                   << (countThread == 1 ? " поток" : (countThread < 5 ? " потока" : " потоков"));
 
         for (auto &countVector : sizeOfVector) {
-            vector1.resize(countVector, 1);
-            vector2.resize(countVector, 2);
-            std::vector<std::thread> threads;
-            double sumOfVectors = 0;
-            std::mutex mtx;
+            std::vector<int> vector1(countVector, 1); // Инициализация значениями 1
+            std::vector<int> vector2(countVector, 2); // Инициализация значениями 2
+            std::vector<int> resultVector(countVector); // Вектор для хранения результатов
 
-            int partOfTheSize = static_cast<int>(countVector / countThread);
+            std::vector<std::thread> threads;
+            size_t partOfTheSize = countVector / countThread;
             auto start = std::chrono::high_resolution_clock::now();
 
-            for (int i = 0; i < countThread; i++) {
-                std::vector<int> newVector1(vector1.begin() + i * partOfTheSize, vector1.begin() +
-                                                                                 (i == countThread - 1 ? countVector : (i + 1) * partOfTheSize));
-                std::vector<int> newVector2(vector2.begin() + i * partOfTheSize, vector2.begin() +
-                                                                                 (i == countThread - 1 ? countVector : (i + 1) * partOfTheSize));
+            for (int i = 0; i < countThread; ++i) {
+                size_t startIdx = i * partOfTheSize;
+                size_t endIdx = (i == countThread - 1) ? countVector : (i + 1) * partOfTheSize;
 
-                threads.push_back(
-                        std::thread(sumVectors, std::ref(sumOfVectors), newVector1, newVector2, std::ref(mtx)));
+                threads.push_back(std::thread(sumVectors, std::ref(vector1), std::ref(vector2),
+                                              std::ref(resultVector), startIdx, endIdx));
             }
 
             for (auto &it : threads) {
